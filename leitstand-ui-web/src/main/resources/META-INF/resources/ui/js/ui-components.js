5721169538a2c314af1916ebffcb56a049609707
Martin Steger <martin@rtbrick.com>
2020-11-26 01:07:21 +0100

    Add 'html' escape function

--- leitstand-ui-web/src/main/resources/META-INF/resources/ui/js/ui-components.js
+++ leitstand-ui-web/src/main/resources/META-INF/resources/ui/js/ui-components.js
@@ -2744,3 +2744,37 @@ customElements.define('ui-label',Label);
 customElements.define('ui-note',Note);
 customElements.define('ui-refresh',RefreshButton);
 customElements.define('ui-paginator',Paginator);
+
+//html escape funtion
+//List of the characters we want to escape and their HTML escaped version
+const chars = {
+  "&": "&amp;",
+  ">": "&gt;",
+  "<": "&lt;",
+  '"': "&quot;",
+  "'": "&#39;",
+  "`": "&#96;"
+};
+
+//Dynamically create a RegExp from the `chars` object
+const re = new RegExp(Object.keys(chars).join("|"), "g");
+
+//Return the escaped string
+const htmlEscape = (str = "") => String(str).replace(re, match => chars[match]);
+
+export const html = (literals, ...substs) => {
+  return literals.raw.reduce((acc, lit, i) => {
+      let subst = substs[i - 1];
+      if (Array.isArray(subst)) {
+          subst = subst.join("");
+      } else if (literals.raw[i - 1] && literals.raw[i - 1].endsWith("$")) {
+          // If the interpolation is preceded by a dollar sign,
+          // substitution is considered safe and will not be escaped
+          acc = acc.slice(0, -1);
+      } else {
+          subst = htmlEscape(subst);
+      }
+
+      return acc + subst + lit;
+  });
+};
